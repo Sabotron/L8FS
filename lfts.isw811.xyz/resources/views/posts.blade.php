2338cba01b0c3781c5989f695c0faf08ba148df0
___hanzel___ <77740697+Sabotron@users.noreply.github.com>
2022-10-16 13:48:15 -0600

    Add files via upload
    Creación de vista "Posts" adaptando plantilla HTML facilitada por el desarrollador. Pantalla de "Post" individuales pendiente (video n°32)

--- lfts.isw811.xyz/resources/views/posts.blade.php
+++ lfts.isw811.xyz/resources/views/posts.blade.php
@@ -1,18 +1,11 @@
 <x-layout>
+    @include('_post-header');
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        @if ($posts->count())
+            <x-posts-grid :posts="$posts"/>
+        @else
+        <p class="text-center">No post yet. Thank you and come again</p>
+        @endif
 
-    @foreach ($posts as $post)
-    <div>
-        <h1>
-            <a href="/posts/{{ $post->slug }}">
-                {{ $post->title}} <!-- blade-->
-            </a>
-        </h1>
-        <p>
-        By <a href="/authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a>
-      </p>
-
-    </div>
-    <div>{{$post->excerpt}}</div>
-    @endforeach
-
+    </main>
 </x-layout>
--- lfts.isw811.xyz/storage/framework/views/1f59a5a444ebd8845f76a2399c6bf8bb7a7dbd21.php
+++ lfts.isw811.xyz/storage/framework/views/1f59a5a444ebd8845f76a2399c6bf8bb7a7dbd21.php
@@ -4,22 +4,46 @@
 <?php if ($component->shouldRender()): ?>
 <?php $__env->startComponent($component->resolveView(), $component->data()); ?>
 <?php $component->withAttributes([]); ?>
+    <?php echo $__env->make('_post-header', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>;
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <?php if($posts->count()): ?>
 
-    <?php $__currentLoopData = $posts; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-    <div>
-        <h1>
-            <a href="/posts/<?php echo e($post->slug); ?>">
-                <?php echo e($post->title); ?> <!-- blade-->
-            </a>
-        </h1>
-        <p>
-        By <a href="/authors/<?php echo e($post->author->username); ?>"><?php echo e($post->author->name); ?></a> in <a href="/categories/<?php echo e($post->category->slug); ?>"><?php echo e($post->category->name); ?></a>
-      </p>
+        <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.post_featured_card','data' => ['post' => $posts[0]]]); ?>
+<?php $component->withName('post_featured_card'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['post' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute($posts[0])]); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
 
-    </div>
-    <div><?php echo e($post->excerpt); ?></div>
-    <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        <?php if($posts->count() > 1): ?>
+        <div class="lg:grid lg:grid-cols-6">
+            <?php $__currentLoopData = $posts->skip(1); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $post): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+            <?php if (isset($component)) { $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4 = $component; } ?>
+<?php $component = $__env->getContainer()->make(Illuminate\View\AnonymousComponent::class, ['view' => 'components.post_card','data' => ['post' => $post,'class' => ''.e($loop->iteration < 3 ? 'col-span-3' : 'col-span-2').'']]); ?>
+<?php $component->withName('post_card'); ?>
+<?php if ($component->shouldRender()): ?>
+<?php $__env->startComponent($component->resolveView(), $component->data()); ?>
+<?php $component->withAttributes(['post' => \Illuminate\View\Compilers\BladeCompiler::sanitizeComponentAttribute($post),'class' => ''.e($loop->iteration < 3 ? 'col-span-3' : 'col-span-2').'']); ?>
+<?php echo $__env->renderComponent(); ?>
+<?php endif; ?>
+<?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
+<?php $component = $__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4; ?>
+<?php unset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4); ?>
+<?php endif; ?>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </div>
+        <?php endif; ?>
+        <?php else: ?>
+        <p class="text-center">No post yet. Thank you and come again</p>
+        <?php endif; ?>
 
+    </main>
  <?php echo $__env->renderComponent(); ?>
 <?php endif; ?>
 <?php if (isset($__componentOriginalc254754b9d5db91d5165876f9d051922ca0066f4)): ?>
